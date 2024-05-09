42b13e870c72127037c51d0e7e480d6d5a4daba5
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-05-09 13:46:17 +0530

    Remove direct database connection, migrate user and blog schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import { Hono } from 'hono';
 import router from './routes/common.routes';
 
-const app = new Hono();
-
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string;
+	};
+}>();
 app.route('/api/v1', router);
 
 export default app;
+	
